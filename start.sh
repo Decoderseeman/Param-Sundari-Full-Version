1bb6457562447f48e0857144166c13eeb95ddb74
Decoderseeman <99487702+Decoderseeman@users.noreply.github.com>
2022-05-25 10:44:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/Decoderseeman/Param-Sundari-Full-Version.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Param
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting 𝙿𝙰𝚁𝙰𝙼 𝚂𝚄𝙽𝙳𝙰𝚁𝙸....🔥"
 python3 bot.py
